bae28773d13fce3df3bac00dfaa5032968324610
Sophie Cornish <sophieahcornish@gmail.com>
2018-09-11 15:37:54 +0100

    added video to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,11 @@ const Home = () => {
 
   return (
 
-    <div> hello </div>
+    <video id="background-video" loop autoPlay>
+      <source src="/assets/low-quality-soma.mp4" type="video/mp4" />
+      {/* <source src="http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4" type="video/ogg" /> */}
+        Your browser does not support the video tag.
+    </video>
   );
 };
 
